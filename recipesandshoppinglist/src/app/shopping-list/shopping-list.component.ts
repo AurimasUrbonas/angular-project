bebfddd66d9c10b0f6457f0ee114151563885a83
Aurimas Urbonas <66870338+AurimasUrbonas@users.noreply.github.com>
2020-06-13 16:50:24 +0300

    Add files via upload

---
+++ recipesandshoppinglist/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { Subscription, Observable } from 'rxjs';
+import { LoggingService } from '../logging.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Observable<{ingredients: Ingredient[] }>;
+  private subscription: Subscription;
+
+  constructor( 
+    private loggingService: LoggingService, 
+    private store: Store<fromApp.AppState>
+  ) { }
+
+  ngOnInit(): void {
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.shoppingListServices.getIngredients();
+    // this.subscription = this.shoppingListServices.ingredientsChanged.subscribe(
+    //   (ingredients: Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // );
+
+    this.loggingService.printLog("Hello from ShoppingListComponent ngOnInit");
+  }
+
+  onEditItem(index: number) {
+    // this.shoppingListServices.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
+  }
+
+  ngOnDestroy() {
+    // this.subscription.unsubscribe();
+  }
+}
